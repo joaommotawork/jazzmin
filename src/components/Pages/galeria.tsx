5dec74b0d05daab1ffddd40a623bdc6cd107e461
João Mota <joaommota.work@gmail.com>
2023-06-20 13:24:38 +0100

    feat(galeria): images concertos

--- src/components/Pages/galeria.tsx
+++ src/components/Pages/galeria.tsx
@@ -4,23 +4,170 @@ import banner1 from '../../../public/images/Banner1.jpeg';
 import banner2 from '../../../public/images/Banner2.jpeg';
 import banner3 from '../../../public/images/Banner3.jpeg';
 import banner4 from '../../../public/images/Banner4.jpeg';
+import forte1 from '../../../public/concertos/24072022MusicaNoForte/Forte1.jpg';
+import forte2 from '../../../public/concertos/24072022MusicaNoForte/Forte2.jpg';
+import forte3 from '../../../public/concertos/24072022MusicaNoForte/Forte3.jpg';
+import forte4 from '../../../public/concertos/24072022MusicaNoForte/Forte4.jpg';
+import forte5 from '../../../public/concertos/24072022MusicaNoForte/Forte5.jpg';
+import forte6 from '../../../public/concertos/24072022MusicaNoForte/Forte6.jpg';
+import forte7 from '../../../public/concertos/24072022MusicaNoForte/Forte7.jpg';
+import forte8 from '../../../public/concertos/24072022MusicaNoForte/Forte8.jpg';
+import forte9 from '../../../public/concertos/24072022MusicaNoForte/Forte9.jpg';
+import forte10 from '../../../public/concertos/24072022MusicaNoForte/Forte10.jpg';
+import forte11 from '../../../public/concertos/24072022MusicaNoForte/Forte11.jpg';
+import forte12 from '../../../public/concertos/24072022MusicaNoForte/Forte12.jpg';
+import forte13 from '../../../public/concertos/24072022MusicaNoForte/Forte13.jpg';
+import forte14 from '../../../public/concertos/24072022MusicaNoForte/Forte14.jpg';
+import forte15 from '../../../public/concertos/24072022MusicaNoForte/Forte15.jpg';
+import forte16 from '../../../public/concertos/24072022MusicaNoForte/Forte16.jpg';
+import forte17 from '../../../public/concertos/24072022MusicaNoForte/Forte17.jpg';
+import forte18 from '../../../public/concertos/24072022MusicaNoForte/Forte18.jpg';
+import forte19 from '../../../public/concertos/24072022MusicaNoForte/Forte19.jpg';
+import festaAcoriana1 from '../../../public/concertos/240822SFestaAcoriana/Festa1.jpg';
+import festaAcoriana2 from '../../../public/concertos/240822SFestaAcoriana/Festa2.jpg';
+import festaAcoriana3 from '../../../public/concertos/240822SFestaAcoriana/Festa3.jpg';
+import festaAcoriana4 from '../../../public/concertos/240822SFestaAcoriana/Festa4.jpg';
+import concertoAudi1 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi1.jpg';
+import concertoAudi2 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi2.jpg';
+import concertoAudi3 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi3.jpg';
+import concertoAudi4 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi4.jpg';
+import concertoAudi5 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi5.jpg';
+import concertoAudi6 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi6.jpg';
+import concertoAudi7 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi7.jpg';
+import concertoAudi8 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi8.jpg';
+import concertoAudi9 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi9.jpg';
+import concertoAudi10 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi10.jpg';
+import concertoAudi11 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi11.jpg';
+import concertoAudi12 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi12.jpg';
+import concertoAudi13 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi13.jpg';
+import concertoAudi14 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi14.jpg';
+import concertoAudi15 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi15.jpg';
+import concertoAudi16 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi16.jpg';
+import concertoAudi17 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi17.jpg';
+import concertoAudi18 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi18.jpg';
+import concertoAudi19 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi19.jpg';
+import concertoAudi20 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi20.jpg';
+import concertoAudi21 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi21.jpg';
+import concertoAudi22 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi22.jpg';
+import concertoAudi23 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi23.jpg';
+import concertoAudi24 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi24.jpg';
+import concertoAudi25 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi25.jpg';
+import concertoAudi26 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi26.jpg';
+import concertoAudi27 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi27.jpg';
+import concertoAudi28 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi28.jpg';
+import concertoAudi29 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi29.jpg';
+import concertoAudi30 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi30.jpg';
+import concertoAudi31 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi31.jpg';
+import concertoAudi32 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi32.jpg';
+import concertoAudi33 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi33.jpg';
+import concertoAudi34 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi34.jpg';
+import concertoAudi35 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi35.jpg';
+import concertoAudi36 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi36.jpg';
+import concertoAudi37 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi37.jpg';
+import concertoAudi38 from '../../../public/concertos/310922ConcertoAuditorioDaMadalenaKateSaragaçoGomes/ConcertoAudi38.jpg';
 import { Navigation } from 'swiper';
 import { useState } from 'react';
 
 type Props = {};
 
 function Galeria({}: Props) {
-	const elementos = [banner1, banner2, banner3, banner4]
+	const elementos = [banner1, banner2, banner3, banner4];
+	const musicaNoForte = [
+		forte1,
+		forte2,
+		forte3,
+		forte4,
+		forte5,
+		forte6,
+		forte7,
+		forte8,
+		forte9,
+		forte9,
+		forte10,
+		forte11,
+		forte12,
+		forte13,
+		forte14,
+		forte15,
+		forte16,
+		forte17,
+		forte18,
+		forte19,
+	];
+	const festaAcoriana = [
+		festaAcoriana1,
+		festaAcoriana2,
+		festaAcoriana3,
+		festaAcoriana4,
+	];
+	const concertoAudi = [
+		concertoAudi1,
+		concertoAudi2,
+		concertoAudi3,
+		concertoAudi4,
+		concertoAudi5,
+		concertoAudi6,
+		concertoAudi7,
+		concertoAudi8,
+		concertoAudi9,
+		concertoAudi10,
+		concertoAudi11,
+		concertoAudi12,
+		concertoAudi13,
+		concertoAudi14,
+		concertoAudi15,
+		concertoAudi16,
+		concertoAudi17,
+		concertoAudi18,
+		concertoAudi19,
+		concertoAudi20,
+		concertoAudi21,
+		concertoAudi22,
+		concertoAudi23,
+		concertoAudi24,
+		concertoAudi25,
+		concertoAudi26,
+		concertoAudi27,
+		concertoAudi28,
+		concertoAudi29,
+		concertoAudi30,
+		concertoAudi31,
+		concertoAudi32,
+		concertoAudi33,
+		concertoAudi34,
+		concertoAudi35,
+		concertoAudi36,
+		concertoAudi37,
+		concertoAudi38,
+	];
 
-	const [slides, setSlides] = useState(elementos)
+	const [slides, setSlides] = useState(elementos);
 
 	return (
-		<div id='galeria' className='w-full h-full p-5 lg:p-20'>
-			<button
-				className='px-4 py-2 font-bold text-white underline bg-blue-500 rounded hover:bg-blue-700'
-				onClick={() => setSlides(elementos)}>
-				Elementos
-			</button>
+		<div id='galeria' className='flex flex-col w-full h-full gap-5 p-5 lg:p-20'>
+			<div className='flex flex-col w-full gap-5 lg:flex-row'>
+				<button
+					className='flex-1 px-4 py-2 font-bold text-white bg-orange-500 rounded cursor-pointer'
+					onClick={() => setSlides(elementos)}>
+					Elementos
+				</button>
+				<button
+					className='flex-1 px-4 py-2 font-bold text-white bg-orange-500 rounded cursor-pointer'
+					onClick={() => setSlides(musicaNoForte)}>
+					Música No Forte | 24/07/2022
+				</button>
+				<button
+					className='flex-1 px-4 py-2 font-bold text-white bg-orange-500 rounded cursor-pointer'
+					onClick={() => setSlides(festaAcoriana)}>
+					Festa Açoriana | 24/08/2022
+				</button>
+				<button
+					className='flex-1 px-4 py-2 font-bold text-white bg-orange-500 rounded cursor-pointer'
+					onClick={() => setSlides(concertoAudi)}>
+					Concerto Auditório da Madalena - Kate Saragaço-Gomes |
+					31/09/22
+				</button>
+			</div>
 			<Swiper
 				className='w-full h-full'
 				spaceBetween={50}
