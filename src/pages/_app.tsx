ab14876d6780eb0cf587b92dc3ae1bbd9dc7109c
João Mota <joaommota.work@gmail.com>
2023-06-21 17:29:50 +0100

    feat(app): metatags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,39 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 				<Head>
 					<title>Jazzmin</title>
 					<link rel='icon' href='/favicon.ico' sizes='any' />
+
+					<meta name='title' content='Jazzmin' />
+					<meta
+						name='description'
+						content='Jazzmin é um colectivo musical que se dedica sobretudo à exploração da música tradicional açoriana, fundindo-a com as sonoridades jazzísticas dos mais emblemático standards do cancioneiro americano e com popular a música francesa. O grupo nasceu na ilha do Pico e é constituído por seis músicos que nela nasceram ou residem.'
+					/>
+
+					<meta property='og:type' content='website' />
+					<meta property='og:url' content='https://www.jazzmin.pt' />
+					<meta property='og:title' content='Jazzmin' />
+					<meta
+						property='og:description'
+						content='Jazzmin é um colectivo musical que se dedica sobretudo à exploração da música tradicional açoriana, fundindo-a com as sonoridades jazzísticas dos mais emblemático standards do cancioneiro americano e com popular a música francesa. O grupo nasceu na ilha do Pico e é constituído por seis músicos que nela nasceram ou residem.'
+					/>
+					<meta property='og:image' content='/images/MetaTags.png' />
+
+					<meta
+						property='twitter:card'
+						content='summary_large_image'
+					/>
+					<meta
+						property='twitter:url'
+						content='https://www.jazzmin.pt'
+					/>
+					<meta property='twitter:title' content='Jazzmin' />
+					<meta
+						property='twitter:description'
+						content='Jazzmin é um colectivo musical que se dedica sobretudo à exploração da música tradicional açoriana, fundindo-a com as sonoridades jazzísticas dos mais emblemático standards do cancioneiro americano e com popular a música francesa. O grupo nasceu na ilha do Pico e é constituído por seis músicos que nela nasceram ou residem.'
+					/>
+					<meta
+						property='twitter:image'
+						content='/images/MetaTags.png'
+					/>
 				</Head>
 				<Component {...pageProps} />
 			</Layout>
